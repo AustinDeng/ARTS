de04c2c5d93c4a006d1843a140bf945c64887f09
tang <tsandxxx@gmail.com>
2019-05-01 12:51:28 +0800

    Add files via upload

---
+++ Tsand/Reflection/模板类的分离与实现/方法二/template_compile.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: template_compile.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年05月01日 星期三 12时30分36秒
+ ************************************************************************/
+
+#ifdef FUCK
+#include<iostream>
+
+using namespace std;
+template<class T>
+T base<T>::add_base(T x,T y){
+    return x+y;
+}
+#endif
