066f72c58a1dbac60e165e29d61e0957a3574f38
tang <tsandxxx@gmail.com>
2019-05-01 12:45:47 +0800

    Add files via upload

---
+++ Tsand/Reflection/模板类的实现与分离/template_compile.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: template_compile.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年05月01日 星期三 12时30分36秒
+*/ 
+
+
+template<class T>
+T base<T>::add_base(T x,T y){
+    return x+y;
+}
+
---
+++ Tsand/Reflection/模板类的实现与分离/use_template.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "template_compile.h"
+using namespace std;
+int main()
+    {
+    base<int> bobj;
+    cout<<bobj.add_base(2,3)<<endl;
+    return 0;
+    }
+
+
